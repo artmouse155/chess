8758c79dda4e44fb3d88db2f75512d32f9d58f5b
Chase Odom <chasemichaelodom@gmail.com>
2025-10-09 11:57:14 -0600

    ✅(data access) Added DataAccess Dummy
    Passed the registration test!

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,10 @@
+package dataaccess;
+
+public class DataAccess {
+    public void createAuth(String username, Object authToken) {
+    }
+
+    public Object getUser(String username) {
+        return null;
+    }
+}
--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -1,18 +1,33 @@
 package service;
 
+import dataaccess.DataAccess;
+import handler.AlreadyTakenException;
+
 import java.util.List;
 import java.util.Map;
 
 public class Service {
 
-    public Service() {}
+    private final DataAccess dataAccess;
+
+    public Service() {
+        dataAccess = new DataAccess();
+    }
 
     public Map<String, String> deleteDB() {
         return Map.of();
     }
 
-    public Map<String, String> register(String username, String password, String email) {
-        return Map.of();
+    public Map<String, String> register(String username, String password, String email) throws AlreadyTakenException {
+
+        var userData = dataAccess.getUser(username);
+        if (userData != null)
+        {
+            throw new AlreadyTakenException("Failed to register username \"" + username + "\". Already taken.");
+        }
+        var authToken = generateAuthToken();
+        dataAccess.createAuth(username, authToken);
+        return Map.of("username", username, "authToken", authToken);
     }
 
     public Map<String, String> login(String username, String password) {
@@ -33,6 +48,11 @@ public class Service {
 
     public Map<String, String> joinGame(String playerColor, int gameID) {
         return Map.of();
+    }
+
+    public String generateAuthToken()
+    {
+        return "Service auth token";
     }
 
 }
