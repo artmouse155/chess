b7c26ca0f90e33e5c3266e045ce61d5a436a6a0e
Chase Odom <chasemichaelodom@gmail.com>
2025-10-09 15:23:44 -0600

    🚧(memoryDB) Using sets to store data

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -5,26 +5,42 @@ import model.GameData;
 import model.UserData;
 
 import java.util.List;
+import java.util.Set;
 
 public class MemoryDataAccess implements DataAccess{
+
+    Set<UserData> userDataSet;
+    Set<AuthData> authDataSet;
+    Set<GameData> gameDataSet;
+
+    public MemoryDataAccess() {
+        userDataSet = Set.of();
+        authDataSet = Set.of();
+        gameDataSet = Set.of();
+    }
+
     @Override
     public void deleteDB() throws DataAccessException {
-        
+        userDataSet.clear();
+        authDataSet.clear();
+        gameDataSet.clear();
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        var filteredStream = userDataSet.stream().filter(userData -> userData.username().equals(username));
+        var first = filteredStream.findFirst();
+        return first.orElse(null);
     }
 
     @Override
     public void createUser(UserData userData) throws DataAccessException {
-
+        userDataSet.add(userData);
     }
 
     @Override
     public void createAuth(AuthData authData) throws DataAccessException {
-
+        authDataSet.add(authData);
     }
 
     @Override
