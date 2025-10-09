4a3c67537c220f8574f1e0de47d9ad75ecb8033f
Chase Odom <chasemichaelodom@gmail.com>
2025-10-09 09:06:54 -0600

    ✅(server) Implemented smart register dummy code

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,10 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
 
 public class Server {
 
@@ -9,12 +13,19 @@ public class Server {
     public Server() {
         server = Javalin.create(config -> config.staticFiles.add("web"));
         server.delete("db",ctx -> ctx.result("{}"));
-        server.post("user", ctx-> ctx.result("{\"username\": \"CHASE_IS COOL\", \"authToken\": 24601}"));
+        server.post("user", this::register);
         server.post("session", ctx -> ctx.result("{\"username\": \"CHASE_IS COOL\", \"authToken\": 24601}"));
         server.delete("session", ctx -> ctx.result("{}"));
         server.get("game", ctx -> ctx.result("{}"));
         server.post("game", ctx -> ctx.result("{}"));
         server.put("game", ctx -> ctx.result("{}"));
+    }
+
+    private void register(Context ctx) {
+        var serializer = new Gson();
+        var req = serializer.fromJson(ctx.body(), Map.class);
+        var res = Map.of("username", req.get("username"), "authToken", "xyz");
+        ctx.result(serializer.toJson(res));
     }
 
     public int run(int desiredPort) {
