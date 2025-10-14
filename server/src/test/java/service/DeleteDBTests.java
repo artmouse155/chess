1484ae4960887cd6eaf76729bf6f339242963c1d
Chase Odom <chasemichaelodom@gmail.com>
2025-10-14 08:10:22 -0600

    ✅(db tests) Added empty DB test

--- server/src/test/java/service/DeleteDBTests.java
+++ server/src/test/java/service/DeleteDBTests.java
@@ -8,8 +8,16 @@ public class DeleteDBTests extends EndpointTests {
 
     @Test
     @Order(1)
+    @DisplayName("Delete empty database")
+    public void deleteEmptyDatabase() {
+        Assertions.assertDoesNotThrow(() -> service.deleteDB());
+        assertEmptyDatabase();
+    }
+
+    @Test
+    @Order(2)
     @DisplayName("Delete non-empty database")
-    public void deleteDatabase() {
+    public void deleteNonEmptyDatabase() {
         Assertions.assertDoesNotThrow(() -> service.register(new UserData("Test McTestFace", "securePassword", "the_real_test_mctestface@test.com")));
         Assertions.assertDoesNotThrow(() -> service.login("Test McTestFace", "securePassword"));
         Assertions.assertDoesNotThrow(() -> service.createGame("Checkers"));
