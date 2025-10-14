becf09cbfe2a44bca60a11070c2a9682a57bdadd
Chase Odom <chasemichaelodom@gmail.com>
2025-10-13 20:44:24 -0600

    ✅(db tests) Adding passing delete DB test!

--- server/src/test/java/service/DeleteDBTests.java
+++ server/src/test/java/service/DeleteDBTests.java
@@ -2,6 +2,7 @@ package service;
 
 import handler.Handler;
 import handler.ResponseException;
+import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.*;
 
@@ -16,9 +17,9 @@ public class DeleteDBTests {
 
     private void assertEmptyDatabase() {
         var db = Assertions.assertDoesNotThrow(() -> handler.handleGetDB(), "handleDeleteDB threw an exception");
-        Assertions.assertTrue(db.get("userDataSet").isEmpty());
-        Assertions.assertTrue(db.get("authDataSet").isEmpty());
-        Assertions.assertTrue(db.get("gameDataSet").isEmpty());
+        Assertions.assertTrue(db.get("userDataSet").isEmpty(), "userDataSet was not empty.");
+        Assertions.assertTrue(db.get("authDataSet").isEmpty(), "authDataSet was not empty.");
+        Assertions.assertTrue(db.get("gameDataSet").isEmpty(), "gameDataSet was not empty.");
     }
 
     // Assert empty database
@@ -28,5 +29,16 @@ public class DeleteDBTests {
         assertEmptyDatabase();
     }
 
+
+    @Test
+    @Order(1)
+    @DisplayName("Delete non-empty database")
+    public void deleteDatabase() {
+        Assertions.assertDoesNotThrow(() -> handler.handleRegister(new UserData("Test McTestFace", "securePassword", "the_real_test_mctestface@test.com")));
+        Assertions.assertDoesNotThrow(() -> handler.handleLogin("Test McTestFace", "securePassword"));
+        Assertions.assertDoesNotThrow(() -> handler.handleCreateGame("Checkers"));
+        Assertions.assertDoesNotThrow(() -> handler.handleDeleteDB());
+        assertEmptyDatabase();
+    }
 
 }
