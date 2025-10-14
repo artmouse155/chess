c91fde161c52d971e41f930937044abaefa18fcd
Chase Odom <chasemichaelodom@gmail.com>
2025-10-14 08:08:21 -0600

    🚧(db test) Refactor to run tests on service instead of handler

--- server/src/test/java/service/DeleteDBTests.java
+++ server/src/test/java/service/DeleteDBTests.java
@@ -1,6 +1,5 @@
 package service;
 
-import handler.Handler;
 import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.*;
@@ -11,10 +10,10 @@ public class DeleteDBTests extends EndpointTests {
     @Order(1)
     @DisplayName("Delete non-empty database")
     public void deleteDatabase() {
-        Assertions.assertDoesNotThrow(() -> handler.handleRegister(new UserData("Test McTestFace", "securePassword", "the_real_test_mctestface@test.com")));
-        Assertions.assertDoesNotThrow(() -> handler.handleLogin("Test McTestFace", "securePassword"));
-        Assertions.assertDoesNotThrow(() -> handler.handleCreateGame("Checkers"));
-        Assertions.assertDoesNotThrow(() -> handler.handleDeleteDB());
+        Assertions.assertDoesNotThrow(() -> service.register(new UserData("Test McTestFace", "securePassword", "the_real_test_mctestface@test.com")));
+        Assertions.assertDoesNotThrow(() -> service.login("Test McTestFace", "securePassword"));
+        Assertions.assertDoesNotThrow(() -> service.createGame("Checkers"));
+        Assertions.assertDoesNotThrow(() -> service.deleteDB());
         assertEmptyDatabase();
     }
 
--- server/src/test/java/service/EndpointTests.java
+++ server/src/test/java/service/EndpointTests.java
@@ -1,21 +1,19 @@
 package service;
 
-import handler.Handler;
-import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.*;
 
 public class EndpointTests {
 
-    protected static Handler handler;
+    protected static Service service;
 
     @BeforeAll
     public static void init() {
-        handler = new Handler();
+        service = new Service();
     }
 
     protected void assertEmptyDatabase() {
-        var db = Assertions.assertDoesNotThrow(() -> handler.handleGetDB(), "handleDeleteDB threw an exception");
+        var db = Assertions.assertDoesNotThrow(() -> service.getDB(), "handleDeleteDB threw an exception");
         Assertions.assertTrue(db.get("userDataSet").isEmpty(), "userDataSet was not empty.");
         Assertions.assertTrue(db.get("authDataSet").isEmpty(), "authDataSet was not empty.");
         Assertions.assertTrue(db.get("gameDataSet").isEmpty(), "gameDataSet was not empty.");
@@ -24,7 +22,6 @@ public class EndpointTests {
     // Assert empty database
     @BeforeEach
     public void setup() {
-        Assertions.assertDoesNotThrow(() -> handler.handleDeleteDB(), "handleDeleteDB threw an exception");
         assertEmptyDatabase();
     }
 
