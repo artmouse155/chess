6d1f13199ecdddea0c0f5be1590eaae65b8e5e1e
Chase Odom <chasemichaelodom@gmail.com>
2025-10-16 08:54:09 -0600

    🔥(game data stripped) Remove unused branches

--- shared/src/main/java/model/GameDataStripped.java
+++ shared/src/main/java/model/GameDataStripped.java
@@ -1,11 +1,4 @@
 package model;
 
-import com.google.gson.Gson;
-
 public record GameDataStripped(int gameID, String whiteUsername, String blackUsername, String gameName) {
-
-    @Override
-    public String toString() {
-        return new Gson().toJson(this);
-    }
 }
