6801595af1dbe399182cf6af8f7a4b0be7d1abe2
Chase Odom <chasemichaelodom@gmail.com>
2025-10-23 17:18:19 -0600

    🚧(main) Implementing conditions to enable using SQL

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,9 +1,23 @@
 import chess.*;
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
 import server.Server;
 
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        Server server = new Server();
+
+        System.out.println(args.length);
+
+        Server server;
+        if (args.length >= 1 && args[0].equals("sql")) {
+            server = new Server(true);
+        } else {
+            server = new Server();
+        }
+
         server.run(8080);
 
         System.out.println("♕ 240 Chess Server");
