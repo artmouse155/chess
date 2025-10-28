1823affc8f9344081a86b0290babca9182fd649c
Chase Odom <chasemichaelodom@gmail.com>
2025-10-28 08:32:59 -0600

    🧪(dataaccess tests) Added functional getNonEmptyDB

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,10 +1,18 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
 import org.junit.jupiter.api.*;
+
+import java.util.Set;
 
 public class DataAccessTests {
 
     static DataAccess dataAccess;
+    protected static UserData testUser = new UserData("test1234", "soSecure!!!", "test@gmail.com");
+
 
     @BeforeAll
     public static void init() {
@@ -33,6 +41,18 @@ public class DataAccessTests {
     @Order(1)
     @DisplayName("Get non-empty DB")
     public void getNonEmptyDB() {
+
+        final var testAuth = new AuthData("auth1234", testUser.username());
+        final var testGame = new GameData(1111, null, null, "Checkers", new ChessGame());
+
+        Assertions.assertDoesNotThrow(() -> dataAccess.createUser(testUser));
+        Assertions.assertDoesNotThrow(() -> dataAccess.createAuth(testAuth));
+        Assertions.assertDoesNotThrow(() -> dataAccess.addGame(testGame));
+        var db = Assertions.assertDoesNotThrow(() -> dataAccess.getDB(), "getDB threw an exception");
+        Assertions.assertFalse(db.isEmpty());
+        Assertions.assertEquals(db.get("userDataSet"), Set.of(testUser), String.format("userDataSet was not empty. (%s)", db.get("userDataSet").toString()));
+        Assertions.assertEquals(db.get("authDataSet"), Set.of(testAuth), String.format("authDataSet was not empty. (%s)", db.get("authDataSet").toString()));
+        Assertions.assertEquals(db.get("gameDataSet"), Set.of(testGame), String.format("gameDataSet was not empty. (%s)", db.get("gameDataSet").toString()));
 
     }
 
