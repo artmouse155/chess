273b888c0f5d72a2a676b7227f7f3dc1336e100f
Chase Odom <chasemichaelodom@gmail.com>
2025-11-04 09:43:09 -0700

    🚧(client main) Add PetShop code

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,7 +2,16 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String serverUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+
+        try {
+            new ChessClient(serverUrl).run();
+
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
     }
 }
