afcd201a93e9744abef5439c677db0552d37fddf
Chase Odom <chasemichaelodom@gmail.com>
2025-11-06 08:30:04 -0700

    ✨(handler) Adding dummy functions

--- client/src/main/java/client/Handler.java
+++ client/src/main/java/client/Handler.java
@@ -11,4 +11,37 @@ public class Handler {
     public String help(String... params) {
         return "help";
     }
+
+    public String quit(String... params) {
+        return "quit";
+    }
+
+    public String login(String... params) {
+        return "login";
+    }
+
+    public String register(String... params) {
+        return "register";
+    }
+
+    public String logout(String... params) {
+        return "logout";
+    }
+
+    public String createGame(String... params) {
+        return "createGame";
+    }
+
+    public String listGame(String... params) {
+        return "listGame";
+    }
+
+    public String playGame(String... params) {
+        return "playGame";
+    }
+
+    public String observeGame(String... params) {
+        return "observeGame";
+    }
+
 }
