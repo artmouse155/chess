05855deffce83992435f72b8d1be8378b913afec
Chase Odom <chasemichaelodom@gmail.com>
2025-11-08 23:23:22 -0700

    ✨(join game request) Added

---
+++ shared/src/main/java/model/JoinGameRequest.java
@@ -0,0 +1,11 @@
+package model;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+
+public record JoinGameRequest(ChessGame.TeamColor teamColor, int gameID) {
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
