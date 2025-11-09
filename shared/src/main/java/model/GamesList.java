9ed67ae944bd00343f8bfd19bab8c179d71b5f3d
Chase Odom <chasemichaelodom@gmail.com>
2025-11-08 23:00:08 -0700

    ✨(gameslist) Added simply numbered method and server method

--- shared/src/main/java/model/GamesList.java
+++ shared/src/main/java/model/GamesList.java
@@ -2,6 +2,7 @@ package model;
 
 import com.google.gson.Gson;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public record GamesList(Set<GameDataStripped> games) {
@@ -9,6 +10,21 @@ public record GamesList(Set<GameDataStripped> games) {
     @Override
     public String toString() {
         return new Gson().toJson(this);
+    }
+
+    public GamesList simplyNumbered() {
+        var simplyNumberedGames = new HashSet<GameDataStripped>();
+        int index = 0;
+        for (final var game : games) {
+            simplyNumberedGames.add(new GameDataStripped(
+                    index,
+                    game.whiteUsername(),
+                    game.blackUsername(),
+                    game.gameName()
+            ));
+            index++;
+        }
+        return new GamesList(simplyNumberedGames);
     }
 
 }
