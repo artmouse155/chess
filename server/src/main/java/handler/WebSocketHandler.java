48aef69cdd5385c1daca599d79c1733923c65684
Chase Odom <chasemichaelodom@gmail.com>
2025-11-20 14:31:23 -0700

    🚧(connect) Trying to implement connect

--- server/src/main/java/handler/WebSocketHandler.java
+++ server/src/main/java/handler/WebSocketHandler.java
@@ -1,18 +1,36 @@
 package handler;
 
 import chess.ChessMove;
+import dataaccess.DataAccessException;
+import handler.exception.InternalServerErrorException;
 import handler.exception.ResponseException;
 import model.AuthData;
+import service.WebSocketService;
 import websocket.messages.ServerMessage;
 
 public class WebSocketHandler {
+
+    private final WebSocketService wsService;
+
+    public WebSocketHandler() {
+        try {
+            wsService = new WebSocketService();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public AuthData handleAuth(String authtoken, int gameID) throws ResponseException {
         return new AuthData(authtoken, "web_socket_handler_username");
     }
 
     public ServerMessage connect(String authtoken, int gameID) throws ResponseException {
-        return new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        try {
+            var chessGame = wsService.connect(authtoken, gameID);
+        } catch (DataAccessException e) {
+            throw new InternalServerErrorException(e.getMessage());
+        }
+        return new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
     }
 
     public ServerMessage makeMove(ChessMove move) throws ResponseException {
---
+++ server/src/main/java/service/WebSocketService.java
@@ -0,0 +1,19 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.SQLDataAccess;
+
+public class WebSocketService {
+
+    private final DataAccess dataAccess;
+
+    public WebSocketService() throws DataAccessException {
+        dataAccess = new SQLDataAccess();
+    }
+
+    public ChessGame connect(String authtoken, int gameID) throws DataAccessException {
+        return dataAccess.getGame(gameID).game();
+    }
+}
