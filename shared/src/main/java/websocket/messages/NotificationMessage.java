821ce53b7789d6efdf1776d5d92261e97a4b3db7
Chase Odom <chasemichaelodom@gmail.com>
2025-11-20 14:37:35 -0700

    🚧(messages) Adding the `extend` statements

--- shared/src/main/java/websocket/messages/NotificationMessage.java
+++ shared/src/main/java/websocket/messages/NotificationMessage.java
@@ -1,10 +1,11 @@
 package websocket.messages;
 
-public class NotificationMessage {
+public class NotificationMessage extends ServerMessage {
 
     private final String message;
 
     public NotificationMessage(String message) {
+        super(ServerMessageType.NOTIFICATION);
         this.message = message;
     }
 
